f7f1a0de51aabd9cab67552f2793b889780ea4b3
Chris Gillum <cgillum@microsoft.com>
2023-06-23 17:22:33 -0700

    Simplify semantic search design Completed the end-to-end implementation + demo.

--- src/WebJobs.Extensions.OpenAI/EmbeddingsContext.cs
+++ src/WebJobs.Extensions.OpenAI/EmbeddingsContext.cs
@@ -11,4 +11,10 @@ namespace WebJobs.Extensions.OpenAI;
 /// </summary>
 /// <param name="Request">The embeddings request that was sent to OpenAI.</param>
 /// <param name="Response">The embeddings response that was received from OpenAI.</param>
-public record EmbeddingsContext(EmbeddingCreateRequest Request, EmbeddingCreateResponse Response);
+public record EmbeddingsContext(EmbeddingCreateRequest Request, EmbeddingCreateResponse Response)
+{
+    /// <summary>
+    /// Gets the number of embeddings that were returned in the response.
+    /// </summary>
+    public int Count => this.Response.Data.Count;
+}
