887d4687d6db0433e526572206b412890bd1907a
Paul Yuknewicz <paulyuk@Pauls-MacBook-Pro-2.local>
2023-10-30 17:41:21 -0700

    Scaffold of isolated Worker attributes for OpenAI
    Signed-off-by: Paul Yuknewicz <paulyuk@Pauls-MacBook-Pro-2.local>

---
+++ Worker.Extensions.OpenAI/src/EmbeddingsOutputAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.OpenAI;
+
+public class EmbeddingsOutputAttribute : OutputBindingAttribute
+{
+
+}
