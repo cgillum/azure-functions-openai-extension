f6fd9bc1c16e8020ce3ee432e5c3e2e864037b16
Paul Yuknewicz (from Dev Box) <paulyuk@microsoft.com>
2023-11-05 21:08:30 -0800

    Correcting extension attribute for Webjob
    Signed-off-by: Paul Yuknewicz (from Dev Box) <paulyuk@microsoft.com>

--- Worker.Extensions.OpenAI/src/EmbeddingsContext.cs
+++ Worker.Extensions.OpenAI/src/EmbeddingsContext.cs
@@ -4,17 +4,18 @@
 using OpenAI.ObjectModels.RequestModels;
 using OpenAI.ObjectModels.ResponseModels;
 
-namespace Microsoft.Azure.Functions.Worker.Extensions.OpenAI;
-
-/// <summary>
-/// Binding target for the <see cref="EmbeddingsAttribute"/>.
-/// </summary>
-/// <param name="Request">The embeddings request that was sent to OpenAI.</param>
-/// <param name="Response">The embeddings response that was received from OpenAI.</param>
-public record EmbeddingsContext(EmbeddingCreateRequest Request, EmbeddingCreateResponse Response)
+namespace Microsoft.Azure.Functions.Worker.Extensions.OpenAI
 {
-    /// <summary>
-    /// Gets the number of embeddings that were returned in the response.
-    /// </summary>
-    public int Count => this.Response.Data.Count;
+        /// <summary>
+        /// Binding target for the <see cref="EmbeddingsAttribute"/>.
+        /// </summary>
+        /// <param name="Request">The embeddings request that was sent to OpenAI.</param>
+        /// <param name="Response">The embeddings response that was received from OpenAI.</param>
+        public record EmbeddingsContext(EmbeddingCreateRequest Request, EmbeddingCreateResponse Response)
+        {
+        /// <summary>
+        /// Gets the number of embeddings that were returned in the response.
+        /// </summary>
+        public int Count => this.Response.Data.Count;
+        }
 }
--- Worker.Extensions.OpenAI/src/EmbeddingsInputConverter.cs
+++ Worker.Extensions.OpenAI/src/EmbeddingsInputConverter.cs
@@ -14,62 +14,64 @@ using Microsoft.Azure.Functions.Worker.Core;
 using Microsoft.Azure.Functions.Worker.Extensions;
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
-namespace Microsoft.Azure.Functions.Worker.Extensions.OpenAI;
-
-class EmbeddingsInputConverter : IInputConverter
+namespace Microsoft.Azure.Functions.Worker.Extensions.OpenAI
 {
-    readonly IOpenAIServiceProvider serviceProvider;
-    readonly ILogger logger;
 
-    public EmbeddingsInputConverter(IOpenAIServiceProvider serviceProvider, ILoggerFactory loggerFactory)
+    class EmbeddingsInputConverter : IInputConverter
     {
-        this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
-        this.logger = loggerFactory?.CreateLogger<EmbeddingsInputConverter>() ?? throw new ArgumentNullException(nameof(loggerFactory));
-    }
+        readonly IOpenAIServiceProvider serviceProvider;
+        readonly ILogger logger;
 
-    public async ValueTask<ConversionResult> ConvertAsync(ConverterContext context)
-    {
-        return context?.Source switch
+        public EmbeddingsInputConverter(IOpenAIServiceProvider serviceProvider, ILoggerFactory loggerFactory)
         {
-            ModelBindingData binding => ConversionResult.Success(await this.ConvertToEmbeddingsContext(binding)),
-            _ => ConversionResult.Unhandled(),
-        };
-    }
+            this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            this.logger = loggerFactory?.CreateLogger<EmbeddingsInputConverter>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
 
-    async Task<EmbeddingsContext> ConvertToEmbeddingsContext(ModelBindingData binding)
-    {
-        this.logger.LogInformation("converting binding to EmbeddingsContext type");
-        return await this.ConvertCoreAsync(binding);
-    }
+        public async ValueTask<ConversionResult> ConvertAsync(ConverterContext context)
+        {
+            return context?.Source switch
+            {
+                ModelBindingData binding => ConversionResult.Success(await this.ConvertToEmbeddingsContext(binding)),
+                _ => ConversionResult.Unhandled(),
+            };
+        }
 
-    //async Task<string> IAsyncConverter<EmbeddingsInputAttribute, string>.ConvertAsync(
-    //    EmbeddingsInputAttribute input,
-    //    CancellationToken cancellationToken)
-    //{
-    //    EmbeddingsContext response = await this.ConvertCoreAsync(input);
-    //    return JsonSerializer.Serialize(response);
-    //}
+        async Task<EmbeddingsContext> ConvertToEmbeddingsContext(ModelBindingData binding)
+        {
+            this.logger.LogInformation("converting binding to EmbeddingsContext type");
+            return await this.ConvertCoreAsync(binding);
+        }
 
-    async Task<EmbeddingsContext> ConvertCoreAsync(
-        ModelBindingData binding)
-    {
-        // IOpenAIService service = this.serviceProvider.GetService(attribute.Model);
-
-        //EmbeddingCreateRequest request = attribute.BuildRequest();
-        //this.logger.LogInformation("Sending OpenAI embeddings request: {request}", request);
-        //EmbeddingCreateResponse response = await service.Embeddings.CreateEmbedding(
-        //    request);
-        //this.logger.LogInformation("Received OpenAI embeddings response: {response}", response);
-
-        //if (attribute.ThrowOnError && response.Error is not null)
+        //async Task<string> IAsyncConverter<EmbeddingsInputAttribute, string>.ConvertAsync(
+        //    EmbeddingsInputAttribute input,
+        //    CancellationToken cancellationToken)
         //{
-        //    throw new InvalidOperationException(
-        //        $"OpenAI returned an error of type '{response.Error.Type}': {response.Error.Message}");
+        //    EmbeddingsContext response = await this.ConvertCoreAsync(input);
+        //    return JsonSerializer.Serialize(response);
         //}
-        this.logger.LogInformation("getting actual embedding context..");
-        var request = new EmbeddingCreateRequest();
-        var response = new EmbeddingCreateResponse();
-        return new EmbeddingsContext(request, response);
-    }
 
+        async Task<EmbeddingsContext> ConvertCoreAsync(
+            ModelBindingData binding)
+        {
+            // IOpenAIService service = this.serviceProvider.GetService(attribute.Model);
+
+            //EmbeddingCreateRequest request = attribute.BuildRequest();
+            //this.logger.LogInformation("Sending OpenAI embeddings request: {request}", request);
+            //EmbeddingCreateResponse response = await service.Embeddings.CreateEmbedding(
+            //    request);
+            //this.logger.LogInformation("Received OpenAI embeddings response: {response}", response);
+
+            //if (attribute.ThrowOnError && response.Error is not null)
+            //{
+            //    throw new InvalidOperationException(
+            //        $"OpenAI returned an error of type '{response.Error.Type}': {response.Error.Message}");
+            //}
+            this.logger.LogInformation("getting actual embedding context..");
+            var request = new EmbeddingCreateRequest();
+            var response = new EmbeddingCreateResponse();
+            return new EmbeddingsContext(request, response);
+        }
+
+    }
 }
