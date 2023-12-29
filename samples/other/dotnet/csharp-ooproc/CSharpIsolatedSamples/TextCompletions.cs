6b6c44fd866669294a17705b4a9ea45e988f188d
Takeharu.Oshida <georgeosddev@gmail.com>
2023-12-29 09:05:57 +0000

    fix dotnet ooproc TextCompletion example

--- samples/other/dotnet/csharp-ooproc/CSharpIsolatedSamples/TextCompletions.cs
+++ samples/other/dotnet/csharp-ooproc/CSharpIsolatedSamples/TextCompletions.cs
@@ -4,8 +4,11 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using OpenAI.ObjectModels.ResponseModels;
+using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
 
 namespace CSharpIsolatedSamples;
+
+
 
 /// <summary>
 /// These samples show how to use the OpenAI Completions APIs. For more details on the Completions APIs, see
@@ -26,22 +29,23 @@ public static class TextCompletions
     }
 
     /// <summary>
-    /// This sample takes a prompt as input, sends it directly to the OpenAI completions API, and results the 
+    /// This sample takes a prompt as input, sends it directly to the OpenAI completions API, and results the
     /// response as the output.
     /// </summary>
     [Function(nameof(GenericCompletion))]
     public static IActionResult GenericCompletion(
-        [HttpTrigger(AuthorizationLevel.Function, "post")] PromptPayload payload,
+        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
+        [FromBody] PromptPayload payload,
         [TextCompletionInput("{Prompt}", Model = "text-davinci-003")] CompletionCreateResponse response,
-        ILogger log)
+        FunctionContext executionContext)
     {
+        ILogger logger = executionContext.GetLogger(nameof(GenericCompletion));
         if (!response.Successful)
         {
             Error error = response.Error ?? new Error() { MessageObject = "OpenAI returned an unspecified error" };
             return new ObjectResult(error) { StatusCode = 500 };
         }
-
-        log.LogInformation("Prompt = {prompt}, Response = {response}", payload.Prompt, response);
+        logger.LogInformation("Prompt = {Prompt}, Response = {response}", payload.Prompt, response);
         string text = response.Choices[0].Text;
         return new OkObjectResult(text);
     }
